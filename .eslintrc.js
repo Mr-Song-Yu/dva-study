65b25d08ecf383cd268066248b5b24b6b5ebbede
Mr-Song-Yu <468721195@qq.com>
2021-07-20 20:05:45 +0800

    代码格式化提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,4 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    sourceType: "module",
-  },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["prettier", "plugin:prettier/recommended"],
   rules: {},
 };
